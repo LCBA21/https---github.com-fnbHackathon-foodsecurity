eed537366f80a9f935c388883111ba4981478089
Lesibana Ngoepe <LCBA21>
2024-10-26 00:42:52 +0200

    first commit

---
+++ foodsecurity/src/main/java/com/algoexperts/foodsecurity/FoodsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.algoexperts.foodsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodsecurityApplication.class, args);
+	}
+
+}
---
+++ foodsecurity/src/test/java/com/algoexperts/foodsecurity/FoodsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algoexperts.foodsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
